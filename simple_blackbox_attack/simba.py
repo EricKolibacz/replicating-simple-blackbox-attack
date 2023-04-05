18e785b5e0b061ea6f7ce405359e95ad0f69a1c0
= <26793186+EricKolibacz@users.noreply.github.com>
2023-04-05 09:44:18 +0200

    Restructure method file structure
    - and implement logic for failing cartesian tests

--- simple_blackbox_attack/simba.py
+++ simple_blackbox_attack/simba.py
@@ -1,38 +1,8 @@
-"""Method implementation from the paper Simple Black-box Adversarial Attacks (Guo et al. 2019)"""
-import random
-from abc import ABC, abstractmethod
+"""Method proposed by Guo et al."""
 
-import numpy as np
 import torch
 
-
-# pylint: disable-msg=R0903
-class SearchVectors(ABC):
-    """Abstract class for the set of search vectors"""
-
-    @abstractmethod
-    def get_random_vector(self) -> torch.Tensor:
-        """Get a random new search vector (without replacement)
-
-        Returns:
-            torch.Tensor: search vectors
-        """
-
-
-class CartesianSearchVectors(SearchVectors):
-    """Search Vectors with cartesian base."""
-
-    def __init__(self, size: torch.Size) -> None:
-        self.size = size
-        self.vector_dimensions = list(np.arange(0, size.numel()))
-
-    def get_random_vector(self) -> torch.Tensor:
-        if len(self.vector_dimensions) == 0:
-            raise IndexError("No Vectors left")
-        dimension = self.vector_dimensions.pop(random.randrange(len(self.vector_dimensions)))
-        search_vector = torch.zeros((self.size.numel()))
-        search_vector[dimension] = 1
-        return search_vector.reshape(self.size)
+from simple_blackbox_attack.set_interface import SearchVectors
 
 
 def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float) -> torch.Tensor:
@@ -65,6 +35,7 @@ def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_siz
             probability_perturbed, prediction_perturbed = torch.topk(probabilities, 1)
 
             if probability_perturbed < probability:
+                print(probability)
                 pertubation += alpha * search_vector
                 probability = probability_perturbed
                 prediction = prediction_perturbed
