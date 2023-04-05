544ef51e298e6c2f99343a4490d920ba01617aa4
EricKolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-04-05 13:22:23 +0000

    Add gpu support

--- simple_blackbox_attack/simba.py
+++ simple_blackbox_attack/simba.py
@@ -5,7 +5,9 @@ import torch
 from simple_blackbox_attack.set_interface import SearchVectors
 
 
-def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float) -> torch.Tensor:
+def simba(
+    model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float, device: str = "cpu"
+) -> torch.Tensor:
     """Adversarial method from the paper Simple Black-box Adversarial Attacks (Guo et al. 2019)
     Named how it is named in the paper
 
@@ -15,18 +17,19 @@ def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_siz
         label (int): correct label of the image (ground truth)
         basis (SearchVectors): set of orthogonal search vectors
         step_size (float): the magnitude of the image pertubation in search vector direction
+        device (str): the device the tensor and model will be stored, default 'cpu'
 
     Returns:
         torch.Tensor: adversarial image
     """
-    pertubation: torch.Tensor = torch.zeros(image.shape)
+    pertubation: torch.Tensor = torch.zeros(image.shape).to(device)
 
-    logits = model(image.unsqueeze(0))
+    logits = model(image.unsqueeze(0).to(device))
     probabilities = torch.nn.functional.softmax(logits[0], dim=0)
     probability, prediction = torch.topk(probabilities, 1)
 
     while prediction.item() == label:
-        search_vector = basis.get_random_vector()
+        search_vector = basis.get_random_vector().to(device)
         for alpha in [-step_size, step_size]:
             pertubed_image: torch.Tensor = image + pertubation + alpha * search_vector
 
