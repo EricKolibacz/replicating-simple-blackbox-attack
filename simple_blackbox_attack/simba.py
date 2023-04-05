35bc58e03de6a5fa2991b0020d20bb240dec37c1
EricKolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-04-05 13:30:41 +0000

    Remove derivable parameter

--- simple_blackbox_attack/simba.py
+++ simple_blackbox_attack/simba.py
@@ -5,9 +5,7 @@ import torch
 from simple_blackbox_attack.set_interface import SearchVectors
 
 
-def simba(
-    model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float, device: str = "cpu"
-) -> torch.Tensor:
+def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float) -> torch.Tensor:
     """Adversarial method from the paper Simple Black-box Adversarial Attacks (Guo et al. 2019)
     Named how it is named in the paper
 
@@ -17,11 +15,11 @@ def simba(
         label (int): correct label of the image (ground truth)
         basis (SearchVectors): set of orthogonal search vectors
         step_size (float): the magnitude of the image pertubation in search vector direction
-        device (str): the device the tensor and model will be stored, default 'cpu'
 
     Returns:
         torch.Tensor: adversarial image
     """
+    device = model.get_device()
     pertubation: torch.Tensor = torch.zeros(image.shape).to(device)
 
     logits = model(image.unsqueeze(0).to(device))
