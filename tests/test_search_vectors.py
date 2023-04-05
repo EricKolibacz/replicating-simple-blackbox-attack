3abf9587232372deb552fd5951f1306ce5ac5b36
= <26793186+EricKolibacz@users.noreply.github.com>
2023-04-05 10:27:40 +0200

    Create tests for inverse dct for the dct class

--- tests/test_search_vectors.py
+++ tests/test_search_vectors.py
@@ -45,9 +45,51 @@ def test_dct_correctly_initialized():
     DCTSearchVectors(IMAGE.size(), RATIO)
 
 
+def test_dct_get_correct_search_vector():
+    """Testing if dct search vector class returns a valid vector"""
+    base = DCTSearchVectors(IMAGE.size(), RATIO)
+    vector1 = base.get_random_vector()
+    vector2 = base.get_random_vector()
+    vector3 = base.get_random_vector()
+
+    assert np.isclose(vector_norm(vector1).numpy(), 1)
+    assert np.isclose(vector_norm(vector2).numpy(), 1)
+    assert np.isclose(vector_norm(vector3).numpy(), 1)
+
+    assert not vector1.equal(vector2)
+    assert not vector1.equal(vector3)
+    assert not vector2.equal(vector3)
+
+
 def test_dct_vector_size():
     """Testing if dct search vector class returns a vector with correct size"""
     base = DCTSearchVectors(IMAGE.size(), RATIO)
     vector = base.get_random_vector()
 
     assert vector.size() == IMAGE.size()
+
+
+def test_inverse_dct_2d():
+    """Testing if dct class has a valid inverse dct in 2D"""
+    base = DCTSearchVectors(IMAGE.size(), RATIO)
+
+    frequency_coefficient = np.zeros((3, 2, 2))
+    frequency_coefficient[0, 0, 0] = 1.0
+    image = base.idct_2d(frequency_coefficient)
+
+    assert np.allclose(image[0, :, :], 0.5)
+    assert np.allclose(image[1:2, :, :], 0.0)
+
+    frequency_coefficient = np.zeros((3, 2, 2))
+    frequency_coefficient[2, 0, 0] = 1.0
+    image = base.idct_2d(frequency_coefficient)
+
+    assert np.allclose(image[2, :, :], 0.5)
+    assert np.allclose(image[0:1, :, :], 0.0)
+
+    frequency_coefficient = np.zeros((3, 2, 2))
+    frequency_coefficient[0, 1, 0] = 1.0
+    image = base.idct_2d(frequency_coefficient)
+
+    assert np.isclose(image[0, :, :], np.array([[0.5, 0.5], [-0.5, -0.5]])).all()
+    assert np.allclose(image[1:2, :, :], 0.0)
