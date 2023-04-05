7f3bc98b9a5dcf4938d09d081f5b98e865ea2c60
= <26793186+EricKolibacz@users.noreply.github.com>
2023-04-05 11:09:13 +0200

    Include budget, input and step and queries output

--- simple_blackbox_attack/simba.py
+++ simple_blackbox_attack/simba.py
@@ -5,7 +5,9 @@ import torch
 from simple_blackbox_attack.set_interface import SearchVectors
 
 
-def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float) -> torch.Tensor:
+def simba(
+    model, image: torch.Tensor, label: int, basis: SearchVectors, step_size: float, budget: float
+) -> torch.Tensor:
     """Adversarial method from the paper Simple Black-box Adversarial Attacks (Guo et al. 2019)
     Named how it is named in the paper
 
@@ -15,9 +17,12 @@ def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_siz
         label (int): correct label of the image (ground truth)
         basis (SearchVectors): set of orthogonal search vectors
         step_size (float): the magnitude of the image pertubation in search vector direction
+        budget (float): the budget of pertubation allowed
 
     Returns:
-        torch.Tensor: adversarial image
+        torch.Tensor: pertubation
+        int: number of base vectors included in the pertubation
+        int: number of queries sent to the model
     """
     pertubation: torch.Tensor = torch.zeros(image.shape)
 
@@ -25,19 +30,22 @@ def simba(model, image: torch.Tensor, label: int, basis: SearchVectors, step_siz
     probabilities = torch.nn.functional.softmax(logits[0], dim=0)
     probability, prediction = torch.topk(probabilities, 1)
 
-    while prediction.item() == label:
+    steps, queries = 0, 0
+    while prediction.item() == label and steps + 1 < (budget / step_size) ** 2:
         search_vector = basis.get_random_vector()
         for alpha in [-step_size, step_size]:
             pertubed_image: torch.Tensor = image + pertubation + alpha * search_vector
 
+            queries += 1
             logits = model(pertubed_image.unsqueeze(0))
             probabilities = torch.nn.functional.softmax(logits[0], dim=0)
             probability_perturbed, prediction_perturbed = torch.topk(probabilities, 1)
 
             if probability_perturbed < probability:
+                steps += 1
                 print(probability)
                 pertubation += alpha * search_vector
                 probability = probability_perturbed
                 prediction = prediction_perturbed
 
-    return pertubation
+    return pertubation, steps, queries
